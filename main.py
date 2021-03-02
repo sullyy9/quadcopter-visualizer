f67270a11201834d249923a9a9115b3b5fb6bcca
Ryan Sullivan <ryansully96@googlemail.com>
2021-03-02 13:14:36 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3.9
+from tkinter import *
+
+# Set the root widget
+root_widget = Tk()
+
+# define a label widget
+label_widget_1 = Label(root_widget, text="Hello")
+
+# pack it onto the screen
+label_widget_1.pack()
+
+# begin a loop for the root widget
+root_widget.mainloop()
