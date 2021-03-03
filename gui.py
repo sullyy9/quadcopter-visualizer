01806af747bcf56f0c9dfe8b524c18ed964e5b73
Ryan Sullivan <ryansully96@googlemail.com>
2021-03-03 14:19:01 +0000

    Debug frame cleanup
    Cleaned up code.
    Added scrolling to debug window

--- gui.py
+++ gui.py
@@ -4,10 +4,11 @@ from tkinter import *
 
 class Gui:
     gui_running = True
-    new_serial = False
+    serial_port = "None"
+    serial_port_new = False
 
     def __init__(self):
-        # Setup the root widget with 5 cells
+        # Setup the root widget, window size and sizing of each frame through cell weight
         self.root_widget = Tk()
         self.root_widget.title("Quadcopter Visualizer")
         self.root_widget.geometry("1536x1024")
@@ -19,7 +20,7 @@ class Gui:
         self.root_widget.columnconfigure(2, weight=0)
         self.root_widget.columnconfigure(3, weight=5)
 
-        # Setup the serial port section
+        # Setup the serial port 'frame' (not really a frame)
         self.serial_port = "None"
         self.label_serial_port = Label(self.root_widget, width=20, text="Serial Port: " + self.serial_port)
         self.label_serial_port.grid(row=0, column=0, sticky=W)
@@ -34,61 +35,64 @@ class Gui:
         self.visualisation_frame = self.VisualisationFrame(self.root_widget)
         self.graph_frame = self.GraphFrame(self.root_widget)
 
-        # Setup window closed event handler
+        # Setup the event handler for when the gui window is closed
         self.root_widget.protocol("WM_DELETE_WINDOW", self.gui_closed)
 
-    # refresh the gui
+    # Refresh the gui
     def update(self):
         self.root_widget.update_idletasks()
         self.root_widget.update()
 
+    # Handler for the serial port update button
     def button_serial_port_function(self):
         self.serial_port = self.entry_serial_port.get()[:8]
         self.label_serial_port.config(text="Serial Port: " + self.serial_port)
-        self.new_serial = True
+        self.serial_port_new = True
 
-    # return true if the serial port has been updated
-    def serial_port_new(self):
-        return self.new_serial
-
-    # get the currently set serial port
+    # Get the currently set serial port
     def serial_port_get(self):
-        self.new_serial = False
+        self.serial_port_new = False
         return self.serial_port
 
-    # add debug output into the debug frame
+    # Write a string into the debug window
     def serial_insert(self, string):
-        self.debug_frame.debug_insert(string)
+        self.debug_frame.debug_window_write(string)
 
-    # handler for when the gui window has been closed
+    # Handler for when the gui window has been closed
     def gui_closed(self):
         self.gui_running = False
 
+    # Class for the Debug window frame. Contains a scrolling textbox where the raw output from the serial port
+    # will be written.
     class DebugFrame:
-        debug_index = 1.0
+        debug_window_index = 1.0
 
         def __init__(self, parent_widget):
-            # Create raw debug output frame
+            # Create raw debug output frame and title.
             self.frame_debug = LabelFrame(parent_widget)
             self.frame_debug.grid(row=1, column=0, columnspan=3, sticky=N + W + S + E)
             self.frame_debug.rowconfigure(0, weight=0)
             self.frame_debug.rowconfigure(1, weight=1)
             self.frame_debug.columnconfigure(0, weight=0)
             self.frame_debug.columnconfigure(1, weight=1)
-
             self.label_title = Label(self.frame_debug, text="Debug Output")
             self.label_title.grid(row=0, column=1)
 
-            self.scrollbar = Scrollbar(self.frame_debug, width=20)
-            self.scrollbar.grid(row=1, column=0, sticky=W + S + N, ipadx=0)
-            self.text_debug = Text(self.frame_debug, state=DISABLED, yscrollcommand=self.scrollbar.set)
-            self.text_debug.grid(row=1, column=1, sticky=N + S + E + W)
+            # Create the text box and scroll bar for the raw debug window.
+            self.scrollbar_debug_window = Scrollbar(self.frame_debug, width=20)
+            self.scrollbar_debug_window.grid(row=1, column=0, sticky=W + S + N, ipadx=0)
+            self.text_debug_window = Text(self.frame_debug, state=DISABLED, yscrollcommand=self.scrollbar_debug_window.set)
+            self.text_debug_window.grid(row=1, column=1, sticky=N + S + E + W)
+            self.scrollbar_debug_window.config(command=self.text_debug_window.yview)
 
-        def debug_insert(self, string):
-            self.text_debug.config(state=NORMAL)
-            self.text_debug.insert(self.debug_index, string)
-            self.text_debug.config(state=DISABLED)
-            self.debug_index += len(string)
+        # Insert text into the debug window.
+        # Make sure the window scrolls downwards when new text is inserted.
+        def debug_window_write(self, string):
+            self.text_debug_window.config(state=NORMAL)
+            self.text_debug_window.insert(self.debug_window_index, string)
+            self.text_debug_window.config(state=DISABLED)
+            self.text_debug_window.see(END)
+            self.debug_window_index += len(string)
 
     class ErrorFrame:
         def __init__(self, parent_widget):
--- main.py
+++ main.py
@@ -5,36 +5,40 @@ import gui
 
 
 def main():
+    # Setup the gui
     main_gui = gui.Gui()
-    serial_port = None
+    serial_port = serial.Serial(port=None, baudrate=115200)
     time_last_refresh = 0
 
     while main_gui.gui_running is True:
+
         # update the gui and get the newest serial port every 10ms
         if (time.time_ns() - time_last_refresh) > 10000000:
             main_gui.update()
+            if main_gui.serial_port_new is True:
+                serial_port_update(serial_port, main_gui.serial_port_get())
+            time_last_refresh = time.time_ns()
 
-            if main_gui.serial_port_new() is True:
-                try:
-                    serial_port = serial.Serial(port="/dev/" + main_gui.serial_port_get(), baudrate=115200,
-                                                parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE,
-                                                bytesize=serial.EIGHTBITS)
+        # Read data from the serial port, strip any null or carriage return characters
+        if serial_port.port is not None and serial_port.in_waiting > 0:
+            string = serial_port.read(serial_port.in_waiting).translate(None, b"\x00\r")
+            main_gui.serial_insert(string)
 
-                except serial.SerialException:
-                    serial_port = None
-
-                else:
-                    serial_port.flushInput()
-
-        # Read data from the serial port
-        if serial_port is not None and serial_port.in_waiting != 0:
-            print(serial_port.in_waiting)
-            main_gui.serial_insert(serial_port.readline())
-
-    if serial_port is not None:
+    # End of main loop. Cleanup
+    if serial_port.port is not None:
         serial_port.close()
-        print("serial port closed")
 
 
+def serial_port_update(serial_port, new_port):
+    serial_port.close()
+    try:
+        serial_port.port = ("/dev/" + new_port)
+        serial_port.open()
+
+    except serial.SerialException:
+        serial_port.port = None
+
+
+# Execute the main function if this file is run as an application
 if __name__ == "__main__":
     main()
